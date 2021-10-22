2572d0a72988e8fb3882ac30044bfb758bf36c9e
AnthonyVas <vasquezanth@gmail.com>
2021-10-22 00:43:35 -0600

    Guia9

--- Week-11/appWebView/android/app/src/main/java/com/appwebview/MainApplication.java
+++ Week-11/appWebView/android/app/src/main/java/com/appwebview/MainApplication.java
@@ -1,10 +1,9 @@
-package com.navigation;
+package com.appwebview;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -63,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.navigation.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.appwebview.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
